1e38532b3040979b65c190dd5667e35129a7d246
JunKatayama1984 <46908046+JunKatayama1984@users.noreply.github.com>
2022-03-02 19:44:29 +1100

    Update main.tf

--- main.tf
+++ main.tf
@@ -16,3 +16,9 @@ terraform {
 provider "aws" {
   region  = "us-east-1"
 }
+
+resource "aws_vpc" "this" {
+  enable_dns_support   = true
+  enable_dns_hostnames = true
+  cidr_block = "10.0.0.0/16"
+}
