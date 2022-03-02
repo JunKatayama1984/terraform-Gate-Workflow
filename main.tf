24d933ad87bd7375540d3cc9342644b9f0b6ba3c
JunKatayama1984 <46908046+JunKatayama1984@users.noreply.github.com>
2022-03-02 20:33:47 +1100

    Update main.tf

--- main.tf
+++ main.tf
@@ -22,8 +22,3 @@ resource "aws_vpc" "this" {
   enable_dns_hostnames = true
   cidr_block = "10.0.0.0/16"
 }
-
-resource "aws_subnet" "this" {
-  vpc_id     = aws_vpc.this.id
-  cidr_block = "10.0.1.0/24"
-}
