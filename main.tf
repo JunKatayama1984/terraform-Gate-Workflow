18c0e9e3e92a704fa851fe5dbff81992e8eefa08
JunKatayama1984 <46908046+JunKatayama1984@users.noreply.github.com>
2022-03-02 18:34:19 +1100

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,1 +1,15 @@
+terraform {
+  backend "remote" {
+    organization = "example-org-a3f09c"
+    workspaces {
+      name = "Challenge5"
+    }
+  }
+}
+
+resource "null_resource" "example" {
+  triggers = {
+    value = "A example resource that does nothing!"
+  }
+}
 
