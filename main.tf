c388a5b46964539534744abb3ae3a49634487dd5
JunKatayama1984 <46908046+JunKatayama1984@users.noreply.github.com>
2022-03-02 18:51:00 +1100

    Update main.tf

--- main.tf
+++ main.tf
@@ -9,8 +9,13 @@ terraform {
 
 
 
+resource "null_resource" "example" {
+  triggers = {
+    value = "A example resource that does nothing!"
+  }
+}
+
 output "primary_subnet_id" {
   description = "Primary Region Subnet ID: "
   value = var.Test123
 }
-
